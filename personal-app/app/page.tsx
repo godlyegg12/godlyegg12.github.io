bd77909b33e1668a9c8a9c75553cb0de486d9d3b
godlyegg12 <godlyegg12@gmail.com>
2024-10-07 21:29:35 -0400

    Mobile update

--- personal-app/app/page.tsx
+++ personal-app/app/page.tsx
@@ -4,44 +4,42 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="">
-      <header className="flex h-[60px] w-full px-[16px] z-10">
-        <table className="table-auto">
-          <thead>
-          <tr>
-            <th className="align-middle">
-              <div className="w-[45px] h-[45px] p-[4px] ">
-                <Image src="/menu_icon.png" width={40} height={40} alt="Menu"/>
-              </div>
-            </th>
-            <th className="flex flex-row grow justify-start h-[60px] align-middle">
-              <div className="flex px-[14px] py-[18px] ${pathwayGothic.className}">
-                <Image src="/Mytube_logo.png" width={36} height={25} alt="MyTube Logo"/>
-                <p className="text-[30px] font-medium tracking-[-0.08em] leading-[25px]">
-                  MyTube
-                </p>
-              </div>
-            </th>
-          </tr>
-          </thead>
-        </table>
+      <header className="flex flex-row h-[48px] sm:h-[60px] w-full px-[16px] z-10">
+        <div className="flex items-center">
+          <div className="w-[45px] h-[45px] p-[4px]">
+            <Image src="/menu_icon.png" width={40} height={40} alt="Menu"/>
+          </div>
+        </div>
+        <div className="flex flex-row grow justify-start align-middle items-center">
+          <div className="flex px-[14px] ${pathwayGothic.className}">
+            <div className="w-[36px] h-[25px]">
+              <Image src="/Mytube_logo.png" width={640} height={443} alt="MyTube Logo"/>
+            </div>
+            <p className="text-[30px] font-medium tracking-[-0.08em] leading-[25px]">
+              MyTube
+            </p>
+          </div>
+        </div>
       </header>
       <main className="flex flex-col">
         <div className="fixed left-0 bottom-0 top-[60px] w-[72px]">
         </div>
-        <div className="flex flex-col">
-          <div className="px-[100px]">
-            <div className="rounded-[16px] overflow-hidden">
+        <div className="flex flex-col p-[16px] sm:p-0">
+          <div className="sm:px-[100px] mb-[16px]">
+            <div className="rounded-[8px] sm:rounded-[16px] overflow-hidden">
               <Image src="/example_banner.png" width={1707} height={284} alt="Banner"/>
             </div>
           </div>
-          <div className="flex flex-row px-[100px] pt-[16px]">
-            <div className="rounded-[50%] overflow-hidden mr-[16px] w-[160px] h-[160px]">
-              <Image src="/channel_profile.jpg" width={160} height={160} alt="Channel Picture"/>
+          <div className="flex flex-row sm:px-[100px]">
+            <div className="flex mr-[16px]">
+              <div className="rounded-[50%] overflow-hidden w-[72px] h-[72px] sm:w-[160px] sm:h-[160px]">
+                <Image src="/channel_profile.jpg" width={160} height={160} alt="Channel Picture"/>
+              </div>
             </div>
             <div>
               <div className="flex flex-col">
                 <div className="flex flex-row">
-                  <h1 className="flex text-[36px]/[50px] ${roboto.className} font-bold">
+                  <h1 className="flex text-[24px]/[32px] sm:text-[36px]/[50px] ${roboto.className} font-bold">
                     Aidan Griffith
                   </h1>
                   <div className="flex ml-[8px] items-center">
@@ -50,8 +48,16 @@ export default function Home() {
                     </div>
                   </div>
                 </div>
-                <div className="text-[#aaa]">
-                  godlyegg12@gmail.com • 420k subscribers • No projects
+                <div className="block sm:flex text-[#aaa] text-nowrap">
+                  <div className="mt-[2px]">
+                    godlyegg12@gmail.com
+                    <div className="hidden sm:inline">
+                      &nbsp;•&nbsp;
+                    </div>
+                  </div>
+                  <div className="mt-[2px]">
+                    420k subscribers • No projects
+                  </div>
                 </div>
               </div>
             </div>
